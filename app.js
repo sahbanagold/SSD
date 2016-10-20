e9d4f9a9697d35ec74609d74d57d0e7598513f40
Sahbana <sahbanalo@gmail.com>
2016-10-20 21:19:16 +0700

    add controller, config, env

--- app.js
+++ app.js
@@ -1,60 +1,90 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var compression = require('compression');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var flash = require('express-flash');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var dotenv = require('dotenv');
+var mongoose = require('mongoose');
+var passport = require('passport');
+// Load environment variables from .env file
+dotenv.load();
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// Controllers
+var HomeController = require('./controllers/home');
+var userController = require('./controllers/user');
+var contactController = require('./controllers/contact');
+
+// Passport OAuth strategies
+require('./config/passport');
 
 var app = express();
 
-// view engine setup
+
+mongoose.connect(process.env.MONGODB);
+mongoose.connection.on('error', function() {
+  console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
+  process.exit(1);
+});
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.set('port', process.env.PORT || 3000);
+app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(expressValidator());
+app.use(methodOverride('_method'));
+app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
+app.get('/',isLoggedIn, HomeController.index);
+app.get('/contact',isLoggedIn, contactController.contactGet);
+app.post('/contact', contactController.contactPost);
+app.get('/account', userController.ensureAuthenticated, userController.accountGet);
+app.put('/account', userController.ensureAuthenticated, userController.accountPut);
+app.delete('/account', userController.ensureAuthenticated, userController.accountDelete);
+app.get('/signup', userController.signupGet);
+app.post('/signup', userController.signupPost);
+app.get('/login', userController.loginGet);
+app.post('/login', userController.loginPost);
+app.get('/forgot', userController.forgotGet);
+app.post('/forgot', userController.forgotPost);
+app.get('/reset/:token', userController.resetGet);
+app.post('/reset/:token', userController.resetPost);
+app.get('/logout', userController.logout);
+app.get('/unlink/:provider', userController.ensureAuthenticated, userController.unlink);
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location'] }));
+app.get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }));
+app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));
+app.get('/auth/google/callback', passport.authenticate('google', { successRedirect: '/', failureRedirect: '/login' }));
+app.get('/auth/twitter', passport.authenticate('twitter'));
+app.get('/auth/twitter/callback', passport.authenticate('twitter', { successRedirect: '/', failureRedirect: '/login' }));
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+    res.redirect('/login');
+}
+// Production error handler
+if (app.get('env') === 'production') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+    console.error(err.stack);
+    res.sendStatus(err.status || 500);
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
 });
-
 
 module.exports = app;
